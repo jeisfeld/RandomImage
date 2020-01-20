75fbbad910d0604cfb81c4a2354bbdc9d9b5b59c
Jörg Eisfeld <github@jeisfeld.de>
2020-01-20 17:16:40 +0100

    Fixed exception on Android 4.x

--- RandomImageIdea/randomImageLib/src/main/java/de/jeisfeld/randomimage/SelectDirectoryActivity.java
+++ RandomImageIdea/randomImageLib/src/main/java/de/jeisfeld/randomimage/SelectDirectoryActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsListView.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
